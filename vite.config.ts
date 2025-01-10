3bf44e51dba2c2d244d61933cbc837f7be647422
Nilton <nilton242009@hotmail.com>
2025-01-09 22:01:27 -0500

    Add: Vitest and testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig as defineViteConfig, mergeConfig } from "vite";
+import { defineConfig as defineVitestConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
+const viteConfig = defineViteConfig({
   plugins: [react()],
-})
+});
+
+const vitestConfig = defineVitestConfig({
+  test: {
+    globals: true, // Permite usar describe, it, expect sin importarlos
+    environment: "jsdom", // Usa jsdom para emular el entorno del navegador
+    setupFiles: "./src/setupTests.ts", // Archivo de configuración de pruebas
+    include: ["src/**/*.{test,spec}.ts", "src/**/*.{test,spec}.tsx"], // Incluir archivos de prueba
+  },
+});
+
+export default mergeConfig(viteConfig, vitestConfig);
